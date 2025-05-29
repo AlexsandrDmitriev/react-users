6598546538825bfe98728c1f9877886c18955999
Alexander Dmitriev <alexander.dmitriev@aventus.work>
2025-05-29 21:35:38 +0300

    fix path map marker

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,24 @@ import App from './App.jsx'
 import './index.css'
 import 'leaflet/dist/leaflet.css'
 
+// Импортируем объект L из Leaflet
+import L from 'leaflet'
+
+// Импортируем изображения маркеров из пакета Leaflet
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png'
+import shadow from 'leaflet/dist/images/marker-shadow.png'
+
+L.Icon.Default.imagePath = '';
+
+// Переопределяем настройки иконки по умолчанию Leaflet,
+// используя импортированные изображения
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl: icon,
+  shadowUrl: shadow,
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
